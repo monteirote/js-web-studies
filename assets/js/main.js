78b88a12df30d26da80ca84da494fb9ad456884f
Vinicius <vinicius@iconax.com.br>
2023-08-25 17:30:19 -0300

    add cards dinâmicos

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,30 +1,29 @@
-function convertPokemonToLi(pokemon, n) {
+
+function convertPokemonTypesToLi(pokemonTypes) {
+    return pokemonTypes.map((typeSlot) => `<li class="type"> ${typeSlot.type.name} </li>`)
+}
+
+function convertPokemonToLi(pokemon) {
+    const typesLi = convertPokemonTypesToLi(pokemon.types).join('');
     return `
         <li class="pokemon">
-          <span class="number">${ n }</span>
+          <span class="number">${ pokemon.order }</span>
           <span class="name">${ pokemon.name }</span>
 
           <div class="detail">
              <ol class="types">
-              <li class="type">grass</li>
-              <li class="type">poison</li>
+                ${ typesLi }
              </ol>
 
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${ n }.svg" alt="${ pokemon.name }">
+            <img src="${ pokemon.sprites.other.dream_world.front_default }" 
+                alt="${ pokemon.name }">
           </div>
         </li>`
 }
 
 const pokemonList = document.getElementById('pokemonList');
 
-pokeApi.getPokemons().then((pokemons) => {
-    const listItems = [];
-
-    pokemons.map();
-
-    for (let i = 0; i < pokemons.length; i++) {
-        const pokemon = pokemons[i];
-        listItems.push(convertPokemonToLi(pokemon));
-    }
-    // pokemonList.innerHTML += convertPokemonToLi(pokemon, i + 1);
-})
+pokeApi.getPokemons().then((pokemons = []) => {
+    const pokemonsToAdd = pokemons.map(convertPokemonToLi).join('')
+    pokemonList.innerHTML = pokemonsToAdd;
+});
--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,9 +1,24 @@
 const pokeApi = {}
 
-pokeApi.getPokemons = (offset = 0, limit = 10) => {
+function convertJSonDataToPokemon(pokeDetail) {
+    const pokemon = new Pokemon();
+    pokemon.name = pokeDetail.name;
+    pokemon.pknumber = pokeDetail.order;
+    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default;
+    pokemon.types = pokemon.types.typeSlot.type.name;
+    pokemon.type = pokemon.types[0];
+}
+
+pokeApi.getPokemonDetail = (pokemon) => {
+    return fetch(pokemon.url).then((response) => response.json())
+}
+
+pokeApi.getPokemons = (offset = 0, limit = 5) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${ offset }&limit=${ limit }`;
     return fetch(url)
         .then((response) => response.json())
         .then((jsonBody) => jsonBody.results)
-        .catch((error) => console.log(error));
-} 
+        .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
+        .then((detailRequest) => Promise.all(detailRequest))
+        .then((pokemonDetails) => pokemonDetails)
+}
